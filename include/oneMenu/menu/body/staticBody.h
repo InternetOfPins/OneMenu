2aa364bc305c2906f7ab9cbcffe120f50ca5cda1
neu-rah <ruihfazevedo@gmail.com>
2026-06-10 22:52:06 +0000

    cleanup

--- include/oneMenu/menu/body/staticBody.h
+++ include/oneMenu/menu/body/staticBody.h
@@ -1,5 +1,5 @@
 /**
- * @file body.h
+ * @file staticBody.h
  * @author Rui Azevedo (neu-rah) (ruihfazevedo@gmail.com)
  * @brief 
 */
@@ -7,34 +7,43 @@
 #pragma once
 
 namespace oneMenu {
-  // body ------------------------------------
 
   template<typename...> struct StaticBody;
 
-  template<> struct StaticBody<>:Chain<> {
-    template<typename Out> static constexpr bool printBody(Out& out) {return false;}
+  template<> struct StaticBody<> : Chain<> {
+    template<typename Out> static constexpr void printTo(Out&) noexcept {}
   };
 
-  template<typename O,typename... OO>
-  struct StaticBody<O,OO...>:Chain<O,OO...> {
-    using Head=O;
-    using Tail=StaticBody<OO...>;
+  template<typename O, typename... OO>
+  struct StaticBody<O, OO...> : Chain<O, OO...> {
+    using Head = O;
+    using Tail = StaticBody<OO...>;
+    
     Head head;
     Tail tail;
-    template<typename I,typename... II>
-    constexpr StaticBody(I&& o,II&&... oo):head{std::forward<I>(o)},tail{std::forward<II>(oo)...}{}
+
+    template<typename I, typename... II>
+    constexpr StaticBody(I&& o, II&&... oo) 
+      : head{std::forward<I>(o)}, tail{std::forward<II>(oo)...} {}
+
     template<typename... II>
-    constexpr StaticBody(II&&... oo):head{},tail{std::forward<II>(oo)...}{}
-    template<typename Out> bool printBody(Out& out) const {
-      head.print(out);//TODO change to `printItem` or `printTo` verify name available
-      return tail.printBody(out);}
+    constexpr StaticBody(II&&... oo) 
+      : head{}, tail{std::forward<II>(oo)...} {}
+
+    template<typename Out> 
+    constexpr void printTo(Out& out) const noexcept {
+      head.printTo(out);
+      tail.printTo(out);
+    }
   };
 
-  //static body factory ---
-  template<typename... OO> constexpr StaticBody<OO...> staticBody(OO&&... oo) {return {std::forward<OO>(oo)...};}
-};//namespace oneMenu
+  template<typename... OO> 
+  constexpr StaticBody<OO...> staticBody(OO&&... oo) {
+    return {std::forward<OO>(oo)...};
+  }
 
-//rules StaticBody query specialization --
-template<typename Q,typename... OO>
-constexpr const bool hapi::query<Q,oneMenu::StaticBody<OO...>>{(hapi::query<Q,OO>||...)};
+} // namespace oneMenu
 
+// New optimized HAPI query engine syntax
+template<typename Q, typename... OO>
+constexpr inline bool hapi::query<Q, oneMenu::StaticBody<OO...>> = (hapi::query<Q, OO> || ...);
